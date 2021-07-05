607056d4830f9bd16b252a19c1aea56d93e27268
Paul Metz <path1172@ox.ac.uk>
2021-07-05 15:33:51 +0000

    add changes in file

--- python_scripts/Humagne_C.D_validation/Determine_C_or_D_guides.py
+++ python_scripts/Humagne_C.D_validation/Determine_C_or_D_guides.py
@@ -2,19 +2,23 @@
     # on a mac, conversion of excel files to csv files happens with CR (Carriage return; https://leemendelowitz.github.io/blog/remove-carriage-return-control-character.html).
     # You can convert your mac.csv to unix.csv by using this code in the command line: tr '\r' '\n' < DC_Metrorail_Ridership.mac.csv > DC_Metrorail_Ridership.unix.csv
     # now things like head will work, so you can check the file.
+#import packages/modules
+import sys
 
-import csv
+#determine the path of the directory you work in at the moment, whether you are in the cluster or somewhere else.
+print(sys.path[0])
 
-#open and read lines of Humagne C
-ref_Humagne_C_opened = open("/Users/paul/Bioinformatics/python_scripts/Humagne_C.D_validation/Humagne_C.csv")
+# Store the reference for Humagne C in the ref_Humagne_C_opened variable. Here you use the previously established path.
+ref_Humagne_C_opened = open("/private/var/folders/sz/w01k4kqn0cbdn3wf4j2tly7r0000gt/T/25bca222/waxing.path.ox.ac.uk/dunnstore/hassan/paul/Git_repository/python_scripts/Humagne_C.D_validation/Humagne_C.csv")
 ref_Humagne_C = ref_Humagne_C_opened.readlines()
-
-#open and read lines of Humagne D
-ref_Humagne_D_opened = open("/Users/paul/Bioinformatics/python_scripts/Humagne_C.D_validation/Humagne_D.csv")
-ref_Humagne_D = ref_Humagne_D_opened.readlines()
-
 print(ref_Humagne_C)
 
+#open("/private/var/folders/sz/w01k4kqn0cbdn3wf4j2tly7r0000gt/T/25bca222/waxing.path.ox.ac.uk/dunnstore/hassan/paul/Git_repository/python_scripts/Humagne_C.D_validation/Humagne_D.csv")
 
+#ref_Humagne_D_opened = open("/private/var/folders/sz/w01k4kqn0cbdn3wf4j2tly7r0000gt/T/25bca222/waxing.path.ox.ac.uk/dunnstore/hassan/paul/Git_repository/python_scripts/Humagne_C.D_validation/Humagne_D.csv")
+#ref_Humagne_D = ref_Humagne_D_opened.readlines()
+#print(ref_Humagne_D)
 
-print("hello")
+import os
+
+print(os.getcwd())
