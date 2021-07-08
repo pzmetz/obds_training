271ea28257505121b52ff049edb810f74ae458b7
Paul Metz <path1172@ox.ac.uk>
2021-07-08 09:45:03 +0000

    changed key of count_spacers.py

--- python_scripts/count_spacers.py
+++ python_scripts/count_spacers.py
@@ -21,11 +21,10 @@ import numpy as np
 import sys
 import argparse
 
-KEY_REGION_START = 20  # start index of key region
+KEY_REGION_START = 18  # start index of key region
 KEY_REGION_END = 40  # end index of key region
 # identifies sequence before guide to determine guide position
-KEY = "CTTGGAGAACCACCTTGTTG"
-
+KEY = "ATTTCTACTCTTGTAGAT"
 
 def count_spacers(input_file, fastq_file, output_file, guide_g):
     """
