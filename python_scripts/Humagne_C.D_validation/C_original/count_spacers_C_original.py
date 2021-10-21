aa6c9d19a2362516ddce27b91d38fe8ba6bd8302
Paul Metz <path1172@ox.ac.uk>
2021-10-21 16:25:15 +0000

    adding lots of files

--- python_scripts/Humagne_C.D_validation/C_original/count_spacers_C_original.py
+++ python_scripts/Humagne_C.D_validation/C_original/count_spacers_C_original.py
@@ -1,4 +1,3 @@
-# key_index = read_sequence.find(KEY)
 #       if key_index >= 0:
 #           start_index = key_index + KEY_REGION_START + len(KEY)
 #           guide = read_sequence[start_index:(start_index + 20)]
@@ -26,7 +25,7 @@ import sys
 import argparse
 
 KEY_REGION_START = 0  # start index of key region 18
-KEY_REGION_END = 42  # end index of key region 40
+KEY_REGION_END = 43  # end index of key region 40
 # identifies sequence before guide to determine guide position
 KEY = "TTGTGGAAAGGACGAAACACCGGTAATTTCTACTCTTGTAGAT"
 
@@ -45,7 +44,7 @@ def count_spacers(input_file, fastq_file, output_file, guide_g):
     key_not_found = 0  # count of reads where key was not found
 
 	# add 'G' to key sequence if included in library
-	if guide_g:
+    if guide_g:
 		global KEY
 		KEY += "G"
 
@@ -71,10 +70,10 @@ def count_spacers(input_file, fastq_file, output_file, guide_g):
         read_sequence = str.upper(str(record.seq)) #DJB comment - reads sequence and converts it to uppercase
         key_region = read_sequence[KEY_REGION_START:KEY_REGION_END] #DJB - Returns sequence between KEY_REGION_START & KEY_REGION_END.
                                                                     #Has already been identified as being between 20 & 40.
-        key_index = read_sequence.find(KEY)
+        key_index = key_region.find(KEY)
         if key_index >= 0:
-            start_index = key_index + KEY_REGION_START + len(KEY)
-            guide = read_sequence[start_index:(start_index + 22)]
+            start_index = key_index + KEY_REGION_START + len(KEY) #key_index = 0, KEY_REGION_START = 0, len(KEY) = 43
+            guide = read_sequence[start_index:(start_index + 23)]
             print(guide)
             if guide in dictionary:
                 dictionary[guide] += 1
@@ -83,20 +82,6 @@ def count_spacers(input_file, fastq_file, output_file, guide_g):
                 non_perfect_matches += 1
         else:
             key_not_found += 1
-
-#        # I replaced key_region with read_sequence, because the programme should look for the KEY in the entire read and not in the key_region, which does not contain the KEY as defined above.
-#        key_index = read_sequence.find(KEY) #DJB comment - Agreed. Perhaps they mean ADAPTOR when they say KEY.
-#        if key_index >= 0: #DJB comment - This will always be true. Can't see how key_index would ever be less than 0.
-#            # I replaced the section as stated above this script with as it is now.
-#            guide = key_region
-#            print(guide)
-#            if guide in dictionary:
-#                dictionary[guide] += 1
-#                perfect_matches += 1
-#            else:
-#                non_perfect_matches += 1
-#        else:
-#            key_not_found += 1
 
     # create ordered dictionary with guides and respective counts and output as a csv file
     dict_sorted = OrderedDict(sorted(dictionary.items(), key=lambda t: t[0]))
